089da3ec65abf74ecf07283da01855f318420396
Pritam Khose <pritamkhose@gmail.com>
2020-07-05 11:47:43 +0530

    Add room lib

---
+++ app/src/main/java/com/pritam/listapp/database/DatabaseCache.kt
@@ -0,0 +1,28 @@
+package com.pritam.listapp.database
+
+import android.util.Log
+import com.pritam.listapp.retrofit.model.Fact
+import com.pritam.listapp.utils.Constants
+
+// Database cache which will call respective dao methods
+class DatabaseCache(appDatabase: AppDatabase) {
+
+    private val factDao = appDatabase.factDao()
+
+    // insert facts in to database
+    fun insert(facts: MutableList<Fact>) {
+        Log.d(Constants.APP_TAG, "Inserting: ${facts.size} fact")
+        factDao.insert(facts)
+    }
+
+    // get all facts from database
+    fun getAllFacts(): MutableList<Fact> {
+        return factDao.getAllFacts()
+    }
+
+    // delete all facts from database
+    fun deleteAllFacts() {
+        factDao.deleteAllFacts()
+    }
+
+}

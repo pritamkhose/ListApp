dd8aa0ffa65f9a3cecacb0139b4b8546738fb9bf
Pritam Khose <pritamkhose@gmail.com>
2020-07-05 11:52:54 +0530

    updated testcase

--- app/src/androidTest/java/com/pritam/listapp/ui/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/pritam/listapp/ui/activity/MainActivityTest.kt
@@ -2,14 +2,19 @@ package com.pritam.listapp.ui.activity
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.pritam.listapp.R
+import com.pritam.listapp.ui.activity.RecyclerViewItemCountAssertion.Companion.withItemCount
 import com.pritam.listapp.ui.activity.TestUtiliy.*
+import org.hamcrest.CoreMatchers
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
@@ -24,7 +29,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    val WaitTimeout : Long = 10 * 1000 // seconds
+    private val waitTimeout: Long = 10 * 1000 // seconds
 
     @Rule
     @JvmField
@@ -32,12 +37,12 @@ class MainActivityTest {
 
     @Before
     fun setUp() {
-        onView(isRoot()).perform(waitId(R.id.tv_title, WaitTimeout));
+        onView(isRoot()).perform(waitId(R.id.tv_title, waitTimeout))
     }
 
     @Test
     fun mainActivityTest() {
-        var textView = onView(
+        val textView = onView(
             allOf(
                 withId(R.id.tv_title), withText("Beavers"),
                 childAtPosition(
@@ -51,8 +56,11 @@ class MainActivityTest {
             )
         )
         textView.check(matches(withText("Beavers")))
+    }
 
-        textView = onView(
+    @Test
+    fun mainActivityTitleTest() {
+        val textView = onView(
             allOf(
                 withText("About Canada"),
                 childAtPosition(
@@ -71,6 +79,11 @@ class MainActivityTest {
         textView.check(matches(withText("About Canada")))
     }
 
+    @Test
+    fun recyclerViewItemCountTest() {
+        onView(withId(R.id.recyclerView)).check(withItemCount(13))
+    }
+
     private fun childAtPosition(
         parentMatcher: Matcher<View>, position: Int
     ): Matcher<View> {
@@ -86,6 +99,30 @@ class MainActivityTest {
                 return parent is ViewGroup && parentMatcher.matches(parent)
                         && view == parent.getChildAt(position)
             }
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/36399787/how-to-count-recyclerview-items-with-espresso
+class RecyclerViewItemCountAssertion private constructor(private val matcher: Matcher<Int>) :
+    ViewAssertion {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView
+        val adapter = recyclerView.adapter
+        assertThat(adapter!!.itemCount, matcher)
+    }
+
+    companion object {
+
+        fun withItemCount(expectedCount: Int): RecyclerViewItemCountAssertion {
+            return withItemCount(CoreMatchers.`is`(expectedCount))
+        }
+
+        fun withItemCount(matcher: Matcher<Int>): RecyclerViewItemCountAssertion {
+            return RecyclerViewItemCountAssertion(matcher)
         }
     }
 }
